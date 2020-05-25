5289220b6fd48d358f0f20fc7bccdf39e4d2be19
JaradL <jaradl@hotmail.co.uk>
2020-05-25 10:19:06 +1000

    Cleaned up code, added APK

--- app/src/main/java/com/ict342/android/MainActivity.kt
+++ app/src/main/java/com/ict342/android/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.ict342.android
 
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -10,25 +9,10 @@ import kotlinx.android.synthetic.main.activity_fullscreen.*
 import org.jetbrains.anko.doAsync
 
 /**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
+ * A full-screen activity that hides the system UI (i.e.
+ * status bar and navigation/system bar)
  */
 class MainActivity : AppCompatActivity() {
-    private val mHideHandler = Handler()
-    private val mHidePart2Runnable = Runnable {
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-        fullscreen_content.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-    }
-
-    private var mVisible: Boolean = false
-    private val mHideRunnable = Runnable { hide() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,19 +20,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_fullscreen)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        mVisible = true
+        supportActionBar?.hide()
 
-        // Set up the user interaction to manually show or hide the system UI.
-        fullscreen_content.setOnClickListener { toggle() }
+        webview.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 
         doAsync {
             runOnUiThread {
                 // Enable javascript so the website functions
-                fullscreen_content.settings.javaScriptEnabled = true
+                webview.settings.javaScriptEnabled = true
 
                 // Override url loading so that the default web browser
                 // doesn't start when a link is clicked
-                fullscreen_content.webViewClient = object : WebViewClient() {
+                webview.webViewClient = object : WebViewClient() {
                     override fun shouldOverrideUrlLoading(
                         view: WebView,
                         url: String
@@ -60,77 +48,25 @@ class MainActivity : AppCompatActivity() {
 
                     override fun onPageFinished(view: WebView?, url: String?) {
                         super.onPageFinished(view, url)
-                        fullscreen_content.visibility = View.VISIBLE
+                        webview.visibility = View.VISIBLE
                         progress_bar.visibility = View.GONE
                     }
                 }
 
                 // Load the URL for the website
-                fullscreen_content.loadUrl("http://www.ict342.rf.gd/wordpress")
+                webview.loadUrl("http://www.ict342.rf.gd/wordpress")
             }
         }
-
-        mHideHandler.removeCallbacks(mHideRunnable)
-        mHideHandler.postDelayed(mHideRunnable, 0.toLong())
     }
 
-    private fun toggle() {
-        if (mVisible) {
-            hide()
-        } else {
-            show()
-        }
-    }
-
-    private fun hide() {
-        // Hide UI first
-        supportActionBar?.hide()
-        mVisible = false
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY.toLong())
-    }
-
-    override fun onResume() {
-        hide()
-        super.onResume()
-    }
-
-    private fun show() {
-        // Show the system bar
-        fullscreen_content.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        mVisible = true
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable)
-    }
-
-    companion object {
-        /**
-         * Whether or not the system UI should be auto-hidden after
-         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
-         */
-        private const val AUTO_HIDE = true
-
-        /**
-         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
-         * user interaction before hiding the system UI.
-         */
-        private const val AUTO_HIDE_DELAY_MILLIS = 3000
-
-        /**
-         * Some older devices needs a small delay between UI widget updates
-         * and a change of the status and navigation bar.
-         */
-        private const val UI_ANIMATION_DELAY = 300
-    }
-
+    /**
+     * Called when the back key is pressed on the key bar.
+     */
     override fun onBackPressed() {
-        if (fullscreen_content.canGoBack())
-            fullscreen_content.goBack()
-        else
+        // If the webview has a previous page, go back.
+        if (webview.canGoBack())
+            webview.goBack()
+        else //  If the webview has no previous page, let the system handle the back function.
             super.onBackPressed()
     }
 }
